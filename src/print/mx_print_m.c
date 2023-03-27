a8b406bb008a462b8cf37b3000d1e61b2acff54c
Nem-mod <67012818+Nem-mod@users.noreply.github.com>
2023-03-27 19:38:45 +0300

    fix: fix ls -m

--- src/print/mx_print_m.c
+++ src/print/mx_print_m.c
@@ -12,7 +12,11 @@ void mx_print_m(t_shell* shell) {
         for (int k = 0; k < ls_array[i]->elements_count; k++) {
             
             mx_printstr(ls_array[i]->elements[k].name);
-            mx_printstr(", ");
+            if (k + 1  > ls_array[i]->elements_count) {
+                mx_printchar(',');
+            }
+            mx_printchar(' ');
+            
         }
         if (i != shell->ls_count - 1)
             mx_printchar('\n');
