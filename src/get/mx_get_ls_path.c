82a0aa1b10e0b335d139b3e49fa57aa3ecb9784e
Nem-mod <67012818+Nem-mod@users.noreply.github.com>
2023-03-28 18:54:18 +0300

    fix: fix seg fault '/' path

--- src/get/mx_get_ls_path.c
+++ src/get/mx_get_ls_path.c
@@ -4,14 +4,12 @@ char* mx_get_ls_path(char* name) {
     char* temp;
     char* path;
 
-    // mx_printstr("HUI\n");
-    // mx_strcat(str, name);
-    // mx_printstr("HUI\n");
+    if(name[0] == '/') {
+        return  mx_strjoin(name, "/");
+    }
     temp = mx_strjoin("./", name);
-    // mx_printstr("HUI\n");
     path = mx_strjoin(temp, "/");
     mx_strdel(&temp);
-    // mx_printstr("HUI\n");
 
     return path;
 }
